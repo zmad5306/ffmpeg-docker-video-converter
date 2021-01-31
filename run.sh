a926d6c9ec626d95d0ba731c8aefd0908c7a4133
zmad5306 <zmad5306@outlook.com>
2021-01-31 08:04:06 -0500

    fixing bug

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ inotifywait -m --exclude "[^mp4]$" /vids -e close_write -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
         fn=$(echo $file | cut -f 1 -d '.')
-        ffmpeg -y -i "$path$file" -c copy "$path$fn.mkv" &&  rm "$path$file" &
+        ffmpeg -y -i "$path$file" -c copy "$path$fn.mkv" &&  rm "$path$file" & echo "Processing file '$fn' in the background"
 done
